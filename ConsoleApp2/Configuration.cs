0d52fc0949f2f28f3b2b1cba098d39dff7ce7b6f
olashomali98 <66948326+olashomali98@users.noreply.github.com>
2020-06-18 11:44:19 +0300

    initially, the program prints the fields with their names and values, except the array field which am working on. later, it will be a class library program

---
+++ ConsoleApp2/Configuration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Configuration
+    {
+
+
+        public static void main()
+        {
+            var cl = new Configuration(55, " OLA", new[] { "4545", "9999" });
+
+        }
+
+        public Configuration(int version, string domainName, string[] ipAddresses)
+        {
+            Version = version;
+            DomainName = domainName;
+            IpAddresses = ipAddresses;
+        }
+
+        public int Version;
+        public string DomainName;
+        public string[] IpAddresses;
+       
+
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Markup;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Configuration obj = new Configuration(55, "Ola", new[] { "53", "52" });
+
+            Call(obj);
+
+            Console.ReadLine();
+        }
+
+        private static void Call(object obj)
+        {
+            List<string> info = new List<string>();
+
+
+            Type type = obj.GetType();
+            FieldInfo[] fields = type.GetFields();
+            
+            Console.WriteLine("Fields are: ");
+            foreach (FieldInfo myField in fields)
+            {
+              
+
+                var values =myField.GetValue(obj).ToString();
+               
+
+
+                info.Add(values);
+
+                info.Add(myField.Name);
+                info.Add(myField.FieldType.Name);
+            }
+
+            for (int i = 0; i < info.Count; i++)
+            {
+                Console.WriteLine(info[i]);
+                
+            }
+
+         //hey
+        }
+    }
+}
